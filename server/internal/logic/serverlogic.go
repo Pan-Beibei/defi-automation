410ec950250686b745f77eacd779439686f160bc
Diviner <rookiepbb@gmail.com>
2026-04-07 00:03:15 +0800

    init server

---
+++ server/internal/logic/serverlogic.go
@@ -0,0 +1,33 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.10.1
+
+package logic
+
+import (
+	"context"
+
+	"server/internal/svc"
+	"server/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ServerLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewServerLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ServerLogic {
+	return &ServerLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ServerLogic) Server(req *types.Request) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
